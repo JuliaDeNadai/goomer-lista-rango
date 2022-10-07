67d6d32bc13ed435f5835b60b92c0ab077fdd8bf
Julia <denadaijulia@gmail.com>
2022-10-06 23:32:29 -0300

    add category module

---
+++ api/routes/categories.ts
@@ -0,0 +1,15 @@
+import {Router} from "express"
+import { validate } from "express-validation"
+import { CategoryController } from "../controllers/CategoryController"
+
+const categoryController = new CategoryController()
+const routerCategory = Router()
+
+routerCategory.route("/")
+    .post(categoryController.create)
+
+routerCategory.route("/:id")
+    .get(categoryController.get_category)
+    //.put(categoryController.update)
+
+export {routerCategory}
